d9179f046a9a518e5d10348f3ec33bf19583eb1f
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-08-31 12:41:20 +0200

    Add project files.

---
+++ H1 Playlisten/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H1_Playlisten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tracks = new string[]
+            {
+                "Willy Oneka",
+                "The Second Track",
+                "Third Time's The Charm",
+                "Fourk you",
+            };
+
+            LinkedList<string> playlist = new LinkedList<string>(tracks);
+            int totalTrackCount = playlist.Count; // Using variable because playlist.Count changes in my for-loop.
+
+            for (int i = 0; i < totalTrackCount; i++)
+            {
+                Console.WriteLine(playlist.First.Value);
+                playlist.RemoveFirst();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
