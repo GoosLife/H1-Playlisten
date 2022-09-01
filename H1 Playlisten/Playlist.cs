ead9949970e2ee64c7aa1f6c6c755a111f2eec39
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-09-01 12:58:27 +0200

    Implemented track and playlist classes

---
+++ H1 Playlisten/Playlist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Playlisten
+{
+    internal class Playlist
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private LinkedList<Track> tracks;
+
+        public LinkedList<Track> Tracks
+        {
+            get { return tracks; }
+        }
+
+        public Playlist(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ H1 Playlisten/Track.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Playlisten
+{
+    internal class Track
+    {
+        private string songName;
+
+        public string SongName
+        {
+            get { return songName; }
+        }
+
+        private string artist;
+
+        public string Artist
+        {
+            get { return artist; }
+        }
+
+        public Track(string songName, string artist)
+        {
+            this.songName = songName;
+            this.artist = artist;
+        }
+    }
+}
